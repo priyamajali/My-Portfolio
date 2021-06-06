2cb04daf7a9ca5c93bd173bf2a2c2c71f61b97b8
01fe18bcs158 <01fe18bcs158@kletech.ac.in>
2021-06-06 21:04:35 +0530

    react files

---
+++ src/Components/allBlogs.js
@@ -0,0 +1,47 @@
+import blog1 from '../img/blogs/blog1.svg';
+import blog2 from '../img/blogs/blog2.svg';
+import blog3 from '../img/blogs/blog3.svg';
+import blog4 from '../img/blogs/blog4.svg';
+import blog5 from '../img/blogs/blog5.svg';
+
+const blogs = [
+    {
+        id: 1,
+        image: blog1,
+        title: 'What is ReactJS: Introduction To React and Its Features',
+        link: 'https://wordpressdcc.herokuapp.com/post/8/',
+        date : '21',
+        month: 'Mar',
+        year: '2020'
+    },
+    {
+        id: 2,
+        image: blog2,
+        title: 'Why You Should Build Your Portfolio with React',
+        link: 'https://wordpressdcc.herokuapp.com/post/9/',
+        date : '09',
+        month: 'Dec',
+        year: '2019'
+    },
+    {
+        id: 3,
+        image: blog3,
+        title: 'Differences Between React.js and React Native',
+        link: 'https://wordpressdcc.herokuapp.com/post/10/',
+        date : '15',
+        month: 'Jun',
+        year: '2018'
+    },
+    {
+        id: 4,
+        image: blog4,
+        title: 'How to Build an App with React, Express and SQLite',
+        link: 'https://wordpressdcc.herokuapp.com/post/11/',
+        date : '04',
+        month: 'Aug',
+        year: '2020'
+    }
+    
+];
+
+export default blogs;
